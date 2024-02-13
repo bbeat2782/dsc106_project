e5366c8a5c05755e2f5294a52f18394acc505a6d
alaa-fa <103293663+alaa-fa@users.noreply.github.com>
2024-02-12 22:20:20 -0800

    Update Consumption.svelte

--- src/components/Consumption.svelte
+++ src/components/Consumption.svelte
@@ -143,7 +143,7 @@
       tooltip = d3.select(".consumption-plot")
         .append("div")
         .attr("class", "tooltip")
-        .style("position", "absolute")
+        .style("position", "fixed")
         .style("background-color", "white")
         .style("padding", "5px")
         .style("border", "1px solid black")
